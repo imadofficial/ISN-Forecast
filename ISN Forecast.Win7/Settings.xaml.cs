5ad09ff702283b39da313bac0392872b8e0facd5
Imad <59332263+imadofficial@users.noreply.github.com>
2022-09-17 15:44:19 +0200

    Add files via upload

--- ISN Forecast.Win7/Settings.xaml.cs
+++ ISN Forecast.Win7/Settings.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ISN_Forecast.Win7.Diagnostics;
+using ISN_Forecast.Win7.FirstSetup;
+using ISN_Forecast.Win7.API;
+using Newtonsoft.Json;
 
 namespace ISN_Forecast.Win7
 {
@@ -21,20 +26,198 @@ namespace ISN_Forecast.Win7
     /// </summary>
     public partial class Settings : Page
     {
+        public class data
+        {
+            public String OOBESetup { get; set; }
+            public String Language { get; set; }
+            public String Appearance { get; set; }
+            public String TimeFormat { get; set; }
+            public String DateFormat { get; set; }
+            public String Country { get; set; }
+            public String AllowAlerts { get; set; }
+            public String AllowAutoUpdate { get; set; }
+            public String Unit { get; set; }
+
+        }
+
         public Settings()
         {
             InitializeComponent();
+            Lang();
+
+            CheckColorscheme();
+            CheckAutoUpdates();
+            CheckUnit();
+            CheckRegion();
+            CheckTime();
+            CheckSpeed();
+        }
+
+        public void Lang()
+        {
+            var Lang = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Configs.Translations);
+
+            Statusbar.Instance.Status.Text = Lang["Settings"]["Settings"];
+            Status.Text = Lang["Settings"]["Behavior"];
+            UpdatesTitle.Text = Lang["Settings"]["AutoUpdates"];
+            UpdatesDescription.Text = Lang["Settings"]["AutoUpdatesDesc"];
+            UnitTitle.Text = Lang["Settings"]["Unit"];
+            UnitDescription.Text = Lang["Settings"]["UnitDesc"];
+            GPSTitle.Text = Lang["Settings"]["GPS"];
+            GPSDescription.Text = Lang["Settings"]["GPSDesc"];
+
+            RegionAndGeography.Text = Lang["Settings"]["General"];
+            RegionTitle.Text = Lang["Settings"]["Region"];
+            RegionDescription.Text = Lang["Settings"]["RegionDesc"];
+            TimeTitle.Text = Lang["Settings"]["TimeFormat"];
+            TimeDescription.Text = Lang["Settings"]["TimeFormatDesc"];
+            SpeedTitle.Text = Lang["Settings"]["Speed"];
+            SpeedDescription.Text = Lang["Settings"]["SpeedDesc"];
+
+            Appearance.Text = Lang["Settings"]["Appearance"];
+            AccentColorTitle.Text = Lang["Settings"]["Color"];
+            AccentColorDescription.Text = Lang["Settings"]["ColorDesc"];
+
+            Additional.Text = Lang["Settings"]["AdditionalServices"];
+            EASTitle.Text = Lang["Settings"]["EmergencyAlerts"];
+            EASDescription.Text = Lang["Settings"]["EmergencyAlertsDesc"];
+            EEWTitle.Text = Lang["Settings"]["EEW"];
+            EEWDescription.Text = Lang["Settings"]["EEWDesc"];
+
+            Troubleshoot.Text = Lang["Settings"]["DiagCategory"];
+            ConnectTitle.Text = Lang["Settings"]["Diag"];
+            ConnectDescription.Text = Lang["Settings"]["DiagDesc"];
+            ResetTitle.Text = Lang["Settings"]["Reset"];
+            ResetDescription.Text = Lang["Settings"]["ResetDesc"];
+
+            WiiMODE.Text = Lang["Settings"]["Misc"];
+            WiiTitle.Text = Lang["Settings"]["Wii"];
+            WiiDescription.Text = Lang["Settings"]["WiiDesc"];
+        }
+
+        private void CheckSpeed()
+        {
+            var Settings = File.ReadAllText("Assets/Settings.json");
+            var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Settings);
+
+            SpeedSelection.Items.Insert(0, "km/h");
+            SpeedSelection.Items.Insert(1, "mph");
+
+            if (JSON[0]["Speed"] == "kmh")
+            {
+                SpeedSelection.SelectedIndex = 0;
+            }
+            if (JSON[0]["Speed"] == "mph")
+            {
+                SpeedSelection.SelectedIndex = 1;
+            }
+        }
+
+        private void CheckTime()
+        {
+            var Settings = File.ReadAllText("Assets/Settings.json");
+            var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Settings);
+
+            TimeSelection.Items.Insert(0, DateTime.Now.ToString("hh:mm tt")); //12-Hour Time
+            TimeSelection.Items.Insert(1, DateTime.Now.ToString("HH:mm")); //24-Hour Time
+
+            if (JSON[0]["TimeFormat"] == "24")
+            {
+                TimeSelection.SelectedIndex = 1;
+            }
+            if (JSON[0]["TimeFormat"] == "12")
+            {
+                TimeSelection.SelectedIndex = 0;
+            }
+        }
+
+        private void CheckRegion()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var resourceName = "ISN_Forecast.Win7.Assets.Translations." + Configs.Language + ".json";
+
+            using (Stream stream = asm.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8, true))
+            {
+                var Contents = reader.ReadToEnd();
+                var Lang = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Contents);
+
+                RegionSelection.Items.Insert(0, Lang["Countries"]["BE"]);
+                RegionSelection.Items.Insert(1, Lang["Countries"]["ES"]);
+                RegionSelection.Items.Insert(2, Lang["Countries"]["FR"]);
+                RegionSelection.Items.Insert(3, Lang["Countries"]["UK"]);
+                RegionSelection.Items.Insert(4, Lang["Countries"]["NL"]);
+
+                var Settings = File.ReadAllText("Assets/Settings.json");
+                var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Settings);
+
+                String CountryCode = JSON[0]["Country"];
+                var Results = Lang["Countries"][CountryCode];
+
+               RegionSelection.SelectedItem = Results;
+            }
+        }
+        private void CheckUnit()
+        {
+            var Settings = File.ReadAllText("Assets/Settings.json");
+            var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Settings);
+
+            UnitSelection.Items.Insert(0, "°C");
+            UnitSelection.Items.Insert(1, "°F");
+
+            if (JSON[0]["Unit"] == "temp_f")
+            {
+                UnitSelection.SelectedItem = "°F";
+            }
+            if (JSON[0]["Unit"] == "temp_c")
+            {
+                UnitSelection.SelectedItem = "°C";
+            }
+        }
+
+        private void CheckAutoUpdates()
+        {
+            var Settings = File.ReadAllText("Assets/Settings.json");
+            var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Settings);
+            if(JSON[0]["AllowAutoUpdate"] == "True")
+            {
+                UpdatesBox.IsChecked = true;
+            }
+        }
+        private void CheckColorscheme()
+        {
+            ColorPicker.Items.Insert(0, "Colorful");
+            ColorPicker.Items.Insert(1, "Dark");
+            ColorPicker.Items.Insert(2, "Bright");
+
+            if (Configs.Look == "Gradient")
+            {
+                ColorPicker.SelectedIndex = 0;
+            }
+            if (Configs.Look == "Dark")
+            {
+                ColorPicker.SelectedIndex = 1;
+            }
+            if (Configs.Look == "Bright")
+            {
+                ColorPicker.SelectedIndex = 2;
+            }
         }
 
         private void Close_Click(object sender, RoutedEventArgs e)
         {
+
+            DyamicLoading.Load();
+            DyamicLoading.FutureForecasting();
+            var WeatherData = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Weather.FullText);
+            var JSON = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText("Assets/Settings.json"));
+
             Statusbar.Instance.Status.Text = "Current Weather";
             MainWindow.Instance.ButtonedScreen.Margin = new Thickness(0, -10240, 0, 0);
             NewWeather.Instance.Effect = null;
             NewWeather.Instance.ScrollPerms.IsEnabled = true;
 
             Statusbar.Instance.Settings.IsEnabled = true; Statusbar.Instance.Settings.Opacity = 1;
-            Statusbar.Instance.Refresh.IsEnabled = true; Statusbar.Instance.Refresh.Opacity = 1;
             Statusbar.Instance.Globe.IsEnabled = true; Statusbar.Instance.Globe.Opacity = 1;
             Statusbar.Instance.Search.IsEnabled = true; Statusbar.Instance.Search.Opacity = 1;
         }
@@ -53,9 +236,94 @@ namespace ISN_Forecast.Win7
             MainWindow.Instance.MainContents.Content = new Setup();
             Setup.Instance.MainContent.Content = new Splash();
             Setup.Instance.Status.Text = "Please select an action";
-            Setup.Instance.Extra.Text = "";
             Setup.Instance.Progress.Opacity = 0;
             MainWindow.Instance.MainContents.Opacity = 1;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            GetSettings();
+
+            List<data> _data = new List<data>();
+            _data.Add(new data()
+            {
+                OOBESetup = "False",
+                Language = Configs.Language, //ehhhhh
+                Appearance = Configs.Look, //ok
+                TimeFormat = Configs.TimeFormat, //ok
+                DateFormat = Configs.DateFormat, //still needs work
+                Country = Configs.Country, //ok
+                AllowAlerts = Configs.AllowAlerts, //definitely still needs some work
+                AllowAutoUpdate = Configs.AllowAutoUpdate, //ok
+                Unit = Configs.Unit //ok
+            });
+
+            string json = JsonConvert.SerializeObject(_data.ToArray());
+
+            //write string to file
+            System.IO.File.WriteAllText(@"Assets/Settings.json", json);
+            SavedText.Opacity = 1;
+        }
+
+        private void GetSettings()
+        {
+            Configs.AllowAutoUpdate = UpdatesBox.IsChecked.ToString();
+            Configs.Language = "en_US";
+            Configs.DateFormat = "DD/MM/YYYY";
+            GetCountry();
+            GetUnit();
+            GetAppearance();
+            GetTime();
+        }
+        private void GetTime()
+        {
+            if (TimeSelection.SelectedIndex == 0)
+            {
+                Configs.TimeFormat = "12";
+            }
+            if (TimeSelection.SelectedIndex == 1)
+            {
+                Configs.TimeFormat = "24";
+            }
+        }
+
+        private void GetAppearance()
+        {
+            if (ColorPicker.SelectedIndex == 0)
+            {
+                Configs.Look = "Gradient";
+            }
+            if (ColorPicker.SelectedIndex == 1)
+            {
+                Configs.Look = "BlackAndWhite";
+            }
+        }
+
+        private void GetUnit()
+        {
+            if(UnitSelection.SelectedIndex == 0)
+            {
+                Configs.Unit = "temp_c";
+            }
+            if (UnitSelection.SelectedIndex == 1)
+            {
+                Configs.Unit = "temp_f";
+            }
+        }
+
+        private void GetCountry()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var resourceName = "ISN_Forecast.Win7.Assets.Translations." + Configs.Language + ".json";
+
+            using (Stream stream = asm.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8, true))
+            {
+                var Contents = reader.ReadToEnd();
+                var Lang = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Contents);
+
+                Configs.Country = Lang["Countries"][RegionSelection.Text];
+            }
         }
     }
 }
--- ISN Forecast.Win7/Statusbar.xaml.cs
+++ ISN Forecast.Win7/Statusbar.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ISN_Forecast.Win7.FirstSetup;
+using ISN_Forecast.Win7.WeatherComponents;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -11,6 +13,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -44,16 +47,14 @@ namespace ISN_Forecast.Win7
                 Timer.Interval = new TimeSpan(0, 0, 1);
                 Timer.Start();
             }
-
-
             Instance = this;
-            
         }
 
         private void DefaultTimer(object sender, EventArgs e)
         {
             Time.Text = DateTime.Now.ToString("HH:mm");
-            Date.Text = DateTime.UtcNow.ToString("MM/dd/yyyy");
+            string date = DateTime.UtcNow.ToString("dd/MM/yyyy");
+            Date.Text = date;
         }
 
         private void Timer_Click(object sender, EventArgs e)
@@ -66,10 +67,14 @@ namespace ISN_Forecast.Win7
                 if (SettingsJSON[0]["TimeFormat"] == "24")
                 {
                     Time.Text = DateTime.Now.ToString("HH:mm");
+                    string date = DateTime.UtcNow.ToString("MM/dd/yyyy");
+                    Date.Text = date;
                 }
                 if (SettingsJSON[0]["TimeFormat"] == "12")
                 {
                     Time.Text = DateTime.Now.ToString("hh:mm tt");
+                    string date = DateTime.UtcNow.ToString("MM/dd/yyyy");
+                    Date.Text = date;
                 }
             }
             catch (Exception)
@@ -86,7 +91,8 @@ namespace ISN_Forecast.Win7
 
         private void Search_Click(object sender, RoutedEventArgs e)
         {
-            Status.Text = "Search";
+            var Lang = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Configs.Translations);
+            Status.Text = Lang["Search"]["Search"];
 
             BlurEffect myBlurEffect = new BlurEffect(); //Adding the blur effect
             myBlurEffect.Radius = 24;
@@ -99,15 +105,15 @@ namespace ISN_Forecast.Win7
 
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
+            var Lang = (dynamic)Newtonsoft.Json.JsonConvert.DeserializeObject(Configs.Translations);
             BlurEffect myBlurEffect = new BlurEffect(); //Adding the blur effect
             myBlurEffect.Radius = 24;
             NewWeather.Instance.Effect = myBlurEffect;
             NewWeather.Instance.ScrollPerms.IsEnabled = false;
-            Status.Text = "Settings";
+            Status.Text = Lang["Settings"]["Settings"];
 
             Settings.IsEnabled = false; Settings.Opacity = 0;
-            Refresh.IsEnabled = false;  Refresh.Opacity = 0;
-            Globe.IsEnabled = false;    Globe.Opacity = 0.01;
+            Globe.IsEnabled = false;    Globe.Opacity = 0;
             Search.IsEnabled = false;   Search.Opacity = 0;
 
             MainWindow.Instance.ButtonedScreen.Margin = new Thickness(0, 60, 0, 0);
@@ -119,9 +125,99 @@ namespace ISN_Forecast.Win7
             Status.Text = "Globe";
         }
 
-        private void Refresh_Click(object sender, RoutedEventArgs e)
+        private void Cities_Click(object sender, RoutedEventArgs e) //Opens the SidePanel with all the cities you have saved
         {
-            MainWindow.Instance.MainContents.Content = new NewWeather();
+            
+            var ScreenWidth = MainWindow.Instance.Screen.ActualWidth;
+            if (ScreenWidth < 1280)
+            {
+                    MessageBox.Show("Your Screenres is too low, idot.");
+            }
+
+            if(ScreenWidth > 1781)
+            {
+                MainWindow.Instance.MainContents.Width = 1180;
+                NewWeather.Instance.SevenDay.Margin = new Thickness(50, -250, 0, 0);
+                NewWeather.Instance.AQIBox.Margin = new Thickness(0, 10, 210, 0);
+                MainWindow.Instance.MainContents.HorizontalAlignment = HorizontalAlignment.Right;
+                MainWindow.Instance.Sidepanel.Content = new Sidepanel();
+                MainWindow.Instance.Sidepanel.Margin = new Thickness(40, 70, 0, 0);
+            }
+
+
+            if (ScreenWidth >= 1280 && ScreenWidth <= 1780)
+            {
+                Cities.Margin = new Thickness(-60, 6, 10, 0);
+                Cities.Opacity = 0;
+                AppBehavior.Hamburger = "Forkie";
+                MainWindow.Instance.MainContents.Width = 900;
+                MainWindow.Instance.MainContents.HorizontalAlignment = HorizontalAlignment.Right;
+                NewWeather.Instance.SevenDay.Margin = new Thickness(0, -120, 0, 0);
+                NewWeather.Instance.AQIBox.Margin = new Thickness(0, 10, 0, 0);
+
+                MainWindow.Instance.Sidepanel.Content = new Sidepanel();
+                MainWindow.Instance.Sidepanel.Margin = new Thickness(40, 70, 0, 0);
+             }
+        }
+
+        private async void Music_Click(object sender, RoutedEventArgs e)
+        {
+            BlurEffect myBlurEffect = new BlurEffect(); //Adding the blur effect
+            myBlurEffect.Radius = 28;
+            //Setup.Instance.Effect = myBlurEffect;
+
+            QuinticEase b = new QuinticEase();
+            b.EasingMode = EasingMode.EaseInOut;
+
+            DoubleAnimation HeightAnimation = new DoubleAnimation()
+            {
+                To = 80,
+                Duration = TimeSpan.FromSeconds(1),
+                EasingFunction = b
+            };
+
+            DoubleAnimation WidthAnimation = new DoubleAnimation()
+            {
+                To = 250,
+                Duration = TimeSpan.FromSeconds(1),
+                EasingFunction = b
+            };
+
+            ThicknessAnimation MusicTitle = new ThicknessAnimation()
+            {
+                To = new Thickness(0, 0, 0, 0),
+                Duration = TimeSpan.FromSeconds(1),
+                EasingFunction = b
+            };
+            var converter = new System.Windows.Media.BrushConverter();
+
+            TextBlock TimePassed = new TextBlock();
+            TimePassed.Text = "0:58";
+            TimePassed.VerticalAlignment = VerticalAlignment.Top;
+            TimePassed.HorizontalAlignment = HorizontalAlignment.Left;
+            TimePassed.Margin = new Thickness(20, 5, 0, 0);
+            TimePassed.Foreground = (Brush)converter.ConvertFromString("#FFFFFF");
+            TimePassed.FontSize = 14;
+            TimePassed.FontFamily = new FontFamily(new Uri("pack://application:,,,/"), "./Assets/Fonts/Latin-Based/SFPRODISPLAYREGULAR.OTF#SF Pro Display");
+
+            TextBlock TotalMusicTime = new TextBlock();
+            TotalMusicTime.Text = "2:58";
+            TotalMusicTime.VerticalAlignment = VerticalAlignment.Top;
+            TotalMusicTime.HorizontalAlignment = HorizontalAlignment.Right;
+            TotalMusicTime.Margin = new Thickness(0, -16, 20, 0);
+            TotalMusicTime.Foreground = (Brush)converter.ConvertFromString("#FFFFFF");
+            TotalMusicTime.FontSize = 14;
+            TotalMusicTime.FontFamily = new FontFamily(new Uri("pack://application:,,,/"), "./Assets/Fonts/Latin-Based/SFPRODISPLAYREGULAR.OTF#SF Pro Display");
+
+            //Statusbar.Instance.MusicBox.BeginAnimation(Border.HeightProperty, HeightAnimation);
+            //Statusbar.Instance.MusicBox.BeginAnimation(Border.WidthProperty, WidthAnimation);
+            //await Task.Delay(200);
+            //MainWindow.Instance.ButtonedScreen.Margin = new Thickness(0, 0, 0, 0);
+            //MainWindow.Instance.ButtonedScreen.Content = new MusicUX();
+            //MusicControls.Children.Add(TimePassed);
+            //MusicControls.Children.Add(TotalMusicTime);
+
+            
         }
     }
 }
